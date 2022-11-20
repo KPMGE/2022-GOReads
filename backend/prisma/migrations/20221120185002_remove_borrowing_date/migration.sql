d075214b879873ae3e2e067face34e73f3698f39
KPMGE <jesuskevin254@gmail.com>
2022-11-20 15:52:17 -0300

    :fire: remove unused borrowing_date

---
+++ backend/prisma/migrations/20221120185002_remove_borrowing_date/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `borrowing_date` on the `book_borrowings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "book_borrowings" DROP COLUMN "borrowing_date";
