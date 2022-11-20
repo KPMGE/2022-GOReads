8cf359469fe60ae75daec36a35079163ad5514e4
KPMGE <jesuskevin254@gmail.com>
2022-11-20 16:25:00 -0300

    :sparkles: generate user resources

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,9 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { BooksModule } from './books/books.module';
 import { BookBorrowingModule } from './book-borrowing/book-borrowing.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -14,8 +17,10 @@ import { BookBorrowingModule } from './book-borrowing/book-borrowing.module';
       isGlobal: true
     }),
     BooksModule,
-    BookBorrowingModule
+    BookBorrowingModule,
+    UserModule
   ],
-  providers: [PrismaService],
+  providers: [PrismaService, UserService],
+  controllers: [UserController],
 })
 export class AppModule {}
