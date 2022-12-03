cd1bd91ed5e7baab7615dc35e32503eab7546275
leol0ps <leotimao2@hotmail.com>
2022-12-02 23:27:18 -0300

    :sparkles: added person icon

--- frontend/pages/Register.tsx
+++ frontend/pages/Register.tsx
@@ -9,7 +9,7 @@ export const Register = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <span>Login</span>
+        <span>Register</span>
         <Image
           src="/login.svg"
           alt="Picture of the author"
@@ -24,15 +24,22 @@ export const Register = () => {
             <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
             <input type='email'  placeholder='email'/>
           </div>
-
+          <div className={styles.innerInput}>
+            <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
+            <input type='name'  placeholder='name'/>
+          </div>        
           <div className={styles.innerInput}>
             <FontAwesomeIcon className={styles.icon} icon={faLock} />
             <input type='text' placeholder='password'/>
           </div>
+          <div className={styles.innerInput}>
+            <FontAwesomeIcon className={styles.icon} icon={faLock} />
+            <input type='text' placeholder='Retype password'/>
+          </div>
         </div>
 
-        <button>Login</button>
-        <a href='#'>I don't have an account</a>
+        <button>Register</button>
+        <a href='#'>I have an account already</a>
       </div>
     </div>
   )
